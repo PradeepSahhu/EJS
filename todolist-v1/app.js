1fb16722f51e7c621b1ce528d36b7da57e677369
Pradeep Sahu <specialreaderman.com@gmail.com>
2022-02-15 08:20:09 +0530

    Server adding to our todolist

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,51 +1,109 @@
+
+
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 //Local module thats why we are using .js in the end...
-const date = require(__dirname + "/date.js");
+// const date = require(__dirname + "/date.js");
 
 const app = express();
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+// const items = ["Buy Food", "Cook Food", "Eat Food"];
+// const workItems = [];
 
+// ====== App using modules============
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+// ===============connecting Mongoose============⭐
+mongoose.connect("mongodb://localhost:27017/todolistDB");
 
-// =====================home/root route .==================
+
+// ================Mongoose Schema======================⭐
+const listSchema = {
+  name: String, //Data type in Uppercase.
+};
+
+// ===========Mmongoose Model===================
+
+const Item = mongoose.model( "Item", listSchema);
+
+// =============Mongoose Document===========
+const item1 = new Item({
+  name:"Welcome to your todolist!"
+});
+
+const item2 = new Item({
+  name: "Hit the + button to aff a new item."
+});
+
+const item3 = new Item({
+  name:"<------Hit this to delete an item"
+});
+
+const defaultItems = [item1,item2,item3];
+
+
+
+
+
+
+// =====================Home/root route==================
 app.get("/", function(req, res) {
 
-  let day = date.getDate();
+  //mongoose find items from the console log.
+  Item.find({},function(err, foundItems){
 
-
-//===================rendering the EJS================
-      res.render("list", {
-        listTitle: day,
-
-        newListItems: items
+    // Checking if the array is empty.
+    if (foundItems.length === 0) {
+      // Inserting to the model name
+      Item.insertMany(defaultItems, function(err){
+        if(err){
+          console.log(err);
+        } 
+        else {
+          console.log("Successfully saved the default items to DB.")
+        }
       });
-
+      res.redirect("/"); //redirecting to the home route after adding the data in the empty array.
+    } else {
+      // Rendering the EJS.
+    res.render("list", {
+      listTitle: "Today",
+      newListItems: foundItems
     });
+    }
+  });
+});
 
     // ==================post request response==================
   app.post("/", function(request,respond){
 
 //stores users input in variable item.
-    let item = request.body.newItem;
+    let itemName = request.body.newItem;
 
-    if(request.body.list === "Work"){
-      workItems.push(item);
+    //mongoose document.
+    const item = new Item({
+      name: itemName
+    });
 
-      respond.redirect("/work");
+    item.save();
 
-    } else {
-      items.push(item); // and push it in larger array everytime user input any item.
+    respond.redirect("/");
 
-      respond.redirect("/") //after pushing it redirect to the app.get or home/root route.
+  //   if(request.body.list === "Work"){
+  //     workItems.push(item);
 
-    }
+  //     respond.redirect("/work");
+
+  //   } else {
+  //     foundItems.push(item); // and push it in larger array everytime user input any item.⭐
+
+  //     respond.redirect("/") //after pushing it redirect to the app.get or home/root route.
+
+  //   }
   });
 
   //==================== targeting work route================================
